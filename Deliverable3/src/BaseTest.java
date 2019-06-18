d2262cd05d81326bbc4ad04bb076169592f6e4ca
Dustin Iser <diser@asphaltpanthers.com>
2019-06-17 23:15:42 -0400

    Add support for macOS

--- Deliverable3/src/BaseTest.java
+++ Deliverable3/src/BaseTest.java
@@ -1,8 +1,11 @@
+import java.nio.file.Paths;
+
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.google.common.base.Function;
@@ -11,19 +14,22 @@ public class BaseTest {
 	public WebDriver driver;
 	private WebDriverWait wait;
 	
-	private final boolean isWindows = true;
+	private final boolean isWindows = false;
 	private final boolean isFirefox = false;
 	private final boolean isChrome = true;
 	
 	@Before
 	public void setup() {
 		if (isWindows && isFirefox) {
-			System.setProperty("webdriver.gecko.driver", "drivers\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", Paths.get("drivers", "geckodriver.exe").toString());
 			driver = new FirefoxDriver();
 		}
 		else if (isWindows && isChrome) {
-			System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", Paths.get("drivers", "chromedriver.exe").toString());
 			driver = new ChromeDriver();
+		}
+		else if (!isWindows) {
+			driver = new SafariDriver();
 		}
 		wait = new WebDriverWait(driver, 30);
 	}
